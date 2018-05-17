746687deb2ce1a51fe925aa0711787ac2ef677d0
BEN1JEN <benpheard@gmail.com>
2018-05-17 14:44:35 -0600

    upload files

---
+++ func/auth.lua
@@ -0,0 +1,11 @@
+local auth = {}
+local passwords = {}
+
+function auth.authenticate(user, password)
+
+	if passwords[user] == sha256(pasword) then
+
+	end
+
+	return login
+end
---
+++ main.lua
@@ -0,0 +1,9 @@
+auth = require "func/auth"
+login = require "func/login"
+process = require "func/process"
+
+while true do
+
+	process.update()
+
+end
